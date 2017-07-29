9bb99d5e2b3f0799376f7587d8d8d5b70413b558
KAPITAN <cannaxus@gmail.com>
2017-07-29 20:56:23 +0300

    commit comment

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-browser-sync");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-postcss");
+                       
+    grunt.initConfig({
+        less: {
+            style: {
+                files: {
+                    "css/style.css": "less/main.less"
+                }
+            }
+        },
+        
+        postcss: {
+            option: {
+                processors: [
+                    require("autoprefixer")({browsers:
+                        [
+                        "last 1 version",
+                        "last 2 Chrome versions",
+                        "last 2 Firefox versions",
+                        "last 2 Opera versions",
+                        "last 2 Edge version"
+                        ]})
+                ]
+                
+            },
+          
+            style: {src: "css/*.css"} 
+               
+        },
+
+        
+        watch: {
+            style: {
+                files: ["less/**/*.less"],
+                tasks: ["less", "postcss"]
+            }
+        },
+        browserSync: {
+            server: {
+                bsFiles: {
+                    src: ["*.html", "css/*.css"]
+                },
+                options: {
+                    server: "."
+                }
+            }
+        }
+    })
+};
