6df59c298b0ef1bd1b71eb047e7c7e20781717e5
KAPITAN <cannaxus@gmail.com>
2017-08-27 16:15:04 +0300

    add FullPage scroll

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var popup_plus = document.querySelector(".modal");
 var close_plus = document.querySelector(".close-modal");
 var i;
 
+var link_gallery = document.querySelectorAll(".gallery-btn");
+var popup_gallery = document.querySelector(".modal-gallery");
+var close_gallery = document.querySelector(".close-modal-gallery");
 
 for (i = 0; i < link_buy.length; i++) link_buy[i].addEventListener("click", function (event) {
     event.preventDefault();
@@ -17,6 +20,22 @@ window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
         if (popup_plus.classList.contains("modal-go")) {
             popup_plus.classList.remove("modal-go");
+        }
+    };
+});
+
+for (i = 0; i < link_buy.length; i++) link_gallery[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    popup_gallery.classList.add("modal-go");
+});
+close_gallery.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup_gallery.classList.remove("modal-go");
+});
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+        if (popup_gallery.classList.contains("modal-go")) {
+            popup_gallery.classList.remove("modal-go");
         }
     };
 });
